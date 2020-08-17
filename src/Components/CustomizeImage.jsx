94a4485bff4f6050926d503c18fa52330f121c1d
cchakibb <chakibbachir88@gmail.com>
2020-08-17 15:56:47 +0200

    img component

--- src/Components/CustomizeImage.jsx
+++ src/Components/CustomizeImage.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CustomizeImage() {
-  return <div>CustomizeImage Component</div>;
+  const [url, setUrl] = useState("");
+  const [range, setRange] = useState(0);
+
+  const handleUrl = (event) => {
+    const value = event.target.value;
+    setUrl(value);
+  };
+
+  const handleRange = (event) => {
+    const value = event.target.value;
+    setRange(value);
+  };
+
+  return (
+    <div>
+      <h1>Customize Image</h1>
+      <form>
+        <input placeholder="url" type="text" name="url" value={url} onChange={handleUrl} />
+        <input placeholder="range" type="range" name="range" value={range} onChange={handleRange} />
+      </form>
+      <img src={url} alt="description" style={{ width: range, height: range }} />
+      <p>{range}</p>
+    </div>
+  );
 }
 
 export default CustomizeImage;
